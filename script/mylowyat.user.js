74bd38f424fbdea30488ab5ef33afe5036be6018
Fadli Ishak <muhamad.fadli@n2nconnect.com>
2017-03-24 17:22:24 +0800

    Fix Remove banner still showing, use inline css instead.

--- script/mylowyat.user.js
+++ script/mylowyat.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @author         sk3dsu(Fadli Ishak)
 // @name           MyLowyat
-// @version        0.2.1
+// @version        0.2.2
 // @description    Removing some elements of the forum to be a bit cleaner for SFW.
 // @namespace      https://github.com/mfadliishak/lowyatAvatars
 // @icon           https://forum.lowyat.net/favicon.ico
 // @match          http*://forum.lowyat.net/*
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
+// @run-at      document-idle
 // ==/UserScript==
 
 /**
@@ -17,15 +18,15 @@ var avatars = $('.avatarwrap');
 for (i = 0; i < avatars.length; i++)
 {
     var item = avatars[i];
-    item.style = "display:none;";
+    item.style.display = "none";
 }
 
 /**
  * 2. Remove that big banner up there.
  *
  */
-var banner = $('.borderwrap');
-if (banner)
+var banner = $('.borderwrap #logostrip');
+if (banner.length)
 {
-	banner.style = "display:none;";
+	banner.css("display", "none");
 }
