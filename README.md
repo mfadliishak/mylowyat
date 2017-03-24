cebc46e0505400cff1aa83a814281a33446e23a9
Fadli Ishak <muhamad.fadli@n2nconnect.com>
2017-03-24 10:23:52 +0800

    Initial Project.
    1. Removing avatars.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# mylowyat
+# MyLowyat
 Removing some elements of the forum to be a bit cleaner for SFW.
+
+1. Remove all those annoying avatars
---
+++ script/mylowyat.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @author         sk3dsu(Fadli Ishak)
+// @name           MyLowyat
+// @version        0.1
+// @description    Removing some elements of the forum to be a bit cleaner for SFW.
+// @namespace      https://github.com/mfadliishak/lowyatAvatars
+// @icon           https://forum.lowyat.net/favicon.ico
+// @match          http*://forum.lowyat.net/*
+// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js
+// ==/UserScript==
+
+/**
+ * 1. Remove all those annoying avatars!
+ *
+ */
+var avatars = $('.avatarwrap');
+for (i = 0; i < avatars.length; i++)
+{
+    var item = avatars[i];
+    item.style = "display:none;";
+}
